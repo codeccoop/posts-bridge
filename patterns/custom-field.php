896d9313f6b3ca37474c4a79558208705089bf8e
Lucas García <lucas@codeccoop.org>
2024-02-07 01:13:59 +0100

    feat: update patterns and templates

--- patterns/custom-field.php
+++ patterns/custom-field.php
@@ -1,16 +1,24 @@
 <?php
 
 /**
- * Title: Wpct Custom Field
- * Slug: wpct-custom-field
- * Description: Custom field as span
- * keywords: custom-field
- * viewportWidth: 500 
- * source: plugin
+ * Title: Wpct Remote Field
+ * Slug: wpct-remote-field
+ * Description: Remote field as paragraph
+ * Categories: Remote CPT
+ * Keywords: remote field, backend
+ * Viewport Width: 500 
+ * Source: plugin
+ * Post Types: remote-cpt
  */
 
 ?>
 
-<!-- wp:paragraph {"align":"center","placeholder":"Escriu un títol","fontSize":"larg"} -->
-<p class="has-text-align-center has-large-font-size"></p>
+<!-- wp:paragraph {"align":"center","placeholder":"Post excerpt","fontSize":"larg"} -->
+<p class="has-text-align-center has-large-font-size">Post excerpt:</p>
 <!-- /wp:paragraph -->
+
+<!-- wp:shortcode -->
+[remote_field field="post_excerpt"]
+<h2 class="wp-block-heading is-style-default has-accent-3-color has-text-color has-link-color">%s</h2>
+[/remote_field]
+<!-- /wp:shortcode -->
