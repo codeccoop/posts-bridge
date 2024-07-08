43855978c83f4a9ab3088f1d0c944ed0ba5a937b
Lucas García <lucas@codeccoop.org>
2024-07-08 02:38:22 +0200

    feat: version upgrade

--- wpct-remote-cpt.php
+++ wpct-remote-cpt.php
@@ -8,7 +8,7 @@
  * Author URI:      https://www.codeccoop.org
  * Text Domain:     wpct-rcpt
  * Domain Path:     /languages
- * Version:         2.0.0
+ * Version:         2.0.1
  *
  * @package         Wpct_Remote_Cpt
  */
