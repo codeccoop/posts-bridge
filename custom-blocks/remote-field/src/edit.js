e0f73bcb9c09f89290cf70e2c3ff4b9b9d35b01f
Lucas García <lucas@codeccoop.org>
2024-12-03 02:06:19 +0100

    feat: update remote field custom block after shortcode changes

--- custom-blocks/remote-field/src/edit.js
+++ custom-blocks/remote-field/src/edit.js
@@ -6,56 +6,13 @@ import {
 } from "@wordpress/block-editor";
 import { useEntityProp } from "@wordpress/core-data";
 import { PanelBody } from "@wordpress/components";
-import { useEffect, useMemo, useState } from "@wordpress/element";
 
 import "./editor.scss";
-import { PanelRow } from "@wordpress/components";
 
-export default function Edit({
-  attributes,
-  setAttributes,
-  context: { postType, postId },
-}) {
+export default function Edit({ context: { postType, postId } }) {
   const blockProps = useBlockProps();
 
-  const { remotes } = attributes;
-
-  const [meta] = useEntityProp("postType", postType, "meta", postId);
-  const registeredOptions = useMemo(
-    () =>
-      Object.keys(meta).map((remote) => ({
-        label: remote,
-        value: remote,
-      })),
-    [meta]
-  );
-
-  const [registereds, setRegistereds] = useState([]);
-  const [custom, setCustom] = useState("");
-
-  useEffect(() => {
-    const registereds = [];
-    const custom = [];
-    (remotes || "").split(",").forEach((remote) => {
-      if (Object.prototype.hasOwnProperty.call(meta, remote)) {
-        registereds.push(remote);
-      } else {
-        custom.push(remote);
-      }
-    });
-
-    setRegistereds(registereds);
-    setCustom(custom.join(","));
-  }, [remotes, meta]);
-
-  useEffect(() => {
-    const remotes = (custom || "")
-      .split(",")
-      .map((remote) => remote.trim())
-      .concat(registereds)
-      .join(",");
-    setAttributes({ remotes });
-  }, [registereds, custom]);
+  const [meta = {}] = useEntityProp("postType", postType, "meta", postId);
 
   return (
     <>
--- custom-blocks/remote-field/src/render.php
+++ custom-blocks/remote-field/src/render.php
@@ -1,20 +1,14 @@
 <?php
 /**
-* Dynamic Block Template.
-* @param   array $attributes - A clean associative array of block attributes.
-* @param   array $block - All the block settings and attributes.
-* @param   string $content - The block inner HTML (usually empty unless using inner blocks).
-*/
+ * Dynamic Block Template.
+ * @param   array $attributes - A clean associative array of block attributes.
+ * @param   array $block - All the block settings and attributes.
+ * @param   string $content - The block inner HTML (usually empty unless using inner blocks).
+ */
 
 $is_remote_frontend = shortcode_exists('remote_field');
-if ($is_remote_frontend && isset($attributes['remotes'])) {
-    preg_match_all('{{([^]+)}}', $content, $matches);
-    if (!$matches) {
-        echo $content;
-    } else {
-        $remotes = implode(',', array_map(function ($match) {
-            return trim($match);
-        }, $matches[1]));
-        echo do_shortcode("[remote_fields fields='{$remotes}']{$content}[/remote_fields]");
-    }
+if ($is_remote_frontend) {
+    echo do_shortcode("[remote_fields]{$content}[/remote_fields]");
+} else {
+    echo $content;
 }
