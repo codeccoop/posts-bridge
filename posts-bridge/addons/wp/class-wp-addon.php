c23a59336944795c607a98ef37f3d770c5d54efd
Lucas García <lucas@codeccoop.org>
2026-02-14 23:41:17 +0100

    feat: wp data to schema introspection

--- posts-bridge/addons/wp/class-wp-addon.php
+++ posts-bridge/addons/wp/class-wp-addon.php
@@ -112,6 +112,19 @@ class WP_Addon extends Addon {
 			return array();
 		}
 
+		$backend = PBAPI::get_backend( $backend );
+		if ( ! $backend ) {
+			return array();
+		}
+
+		// Try to introspect the response of the GET endpoint.
+		$fields = $this->get_post_fields( $endpoint, $backend, $method );
+		if ( $fields ) {
+			return $fields;
+		}
+
+		// Fallback to endpoint schema introspection. Some fields may not be
+		// registered as endpoint arguments, such as ACF fields.
 		$endpoint  = rtrim( $endpoint, '/' );
 		$endpoints = $this->get_endpoints( $backend );
 
@@ -121,25 +134,20 @@ class WP_Addon extends Addon {
 
 		$namespace = implode( '/', array_slice( $chunks, 0, 2 ) );
 
-		$backend = PBAPI::get_backend( $backend );
-		if ( ! $backend ) {
-			return array();
-		}
-
 		$response = $backend->get( '/wp-json/' . $namespace );
 		if ( is_wp_error( $response ) ) {
 			return array();
 		}
 
 		$fields = array();
-		foreach ( $response['data']['routes'] as $route => $schema ) {
+		foreach ( $response['data']['routes'] as $route => $route_schema ) {
 			if ( ! preg_match( '#^/wp-json' . $route . '$#', $endpoint ) ) {
 				continue;
 			}
 
 			$method_is_allowed = false;
 
-			foreach ( $schema['endpoints'] as $candidate ) {
+			foreach ( $route_schema['endpoints'] as $candidate ) {
 				if ( ! $method_is_allowed && in_array( 'GET', $candidate['methods'], true ) ) {
 					$method_is_allowed = true;
 				}
@@ -166,6 +174,77 @@ class WP_Addon extends Addon {
 				}
 				break;
 			}
+		}
+
+		return OpenAPI::expand_fields_schema( $fields );
+	}
+
+	/**
+	 * Adds fields from the post GET response body to the endpoitn schema fields.
+	 *
+	 * @param string  $endpoint Post list endpoint.
+	 * @param Backend $backend Backend object.
+	 * @param string  $method Introspection HTTP method.
+	 *
+	 * @return array
+	 */
+	private function get_post_fields( $endpoint, $backend, $method ) {
+		if ( 'GET' !== $method ) {
+			return;
+		}
+
+		$response = $backend->get(
+			$endpoint,
+			array(
+				'per_page' => 1,
+				'context'  => 'edit',
+			),
+		);
+
+		if ( is_wp_error( $response ) || empty( $response['data'][0] ) ) {
+			return;
+		}
+
+		$data = $response['data'][0];
+
+		$taxonomies = array( 'tags', 'categories' );
+		if ( isset( $data['_links']['wp:term'] ) ) {
+			foreach ( $data['_links']['wp:term'] as $tax ) {
+				$taxonomies[] = $tax['taxonomy'];
+			}
+		}
+
+		unset( $data['_links'] );
+
+		$fields = $this->data_to_fields( $data, $taxonomies );
+
+		return OpenAPI::expand_fields_schema( $fields );
+	}
+
+	/**
+	 * Format RCPT fetch response data to a list of json schema fields.
+	 *
+	 * @param array $data Loaded data.
+	 * @param array $taxonomies Known post taxonomies names.
+	 *
+	 * @return array
+	 */
+	private static function data_to_fields( $data, $taxonomies = array() ) {
+		$fields = array();
+
+		foreach ( $data as $key => $val ) {
+			$field = array(
+				'name'   => $key,
+				'schema' => wpct_plugin_value_to_json_schema( $val ),
+			);
+
+			if ( in_array( $field['name'], $taxonomies, true ) ) {
+				$field['schema']['items']['type'] = 'string';
+			} elseif ( 'featured_media' === $field['name'] ) {
+				$field['schema']['type'] = 'string';
+			}
+
+			$fields[] = $field;
 		}
 
 		return $fields;
